4f64d36b070763da1623fd13a60a999560460606
4rchive7 <35294921+4rchive7@users.noreply.github.com>
2018-11-14 18:01:04 +0900

    Update wifiState.js

--- SpringServer/Server/web/wifiState.js
+++ SpringServer/Server/web/wifiState.js
@@ -2,17 +2,6 @@ onmessage = function(evt) {
 
 	var curr_val = -1;
 	var new_val = -1;
-//	while (true) {
-//		const http = new XMLHttpRequest();
-//		http.open("GET", "http://70.12.114.144/Server/wifi.do");
-//		new_val = http.send();
-		/*setTimeout(function() {
-			if(curr_val != new_val){
-				curr_val = new_val;
-				postMessage(curr_val);
-			}
-		}, 30000);*/
-
 	console.log("new_val : "+ new_val);
 		setInterval(function() {
 			var xhttp = new XMLHttpRequest();
